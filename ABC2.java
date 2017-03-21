83c791ed8c5d48510803470b529d9197a4daa698
lihui880518 <873257222@qq.com>
2017-03-21 13:44:51 +0800

    Add files via upload
    add ABC2.java

---
+++ ABC2.java
@@ -0,0 +1,8 @@
+public class ABC2{
+	public static void main(String args[]){
+		String letters="ABC";
+		Object numbers=new char[]{''1,'2','3'};
+		System.out.print(letters+"easy as");
+		System.out.println(numbers);
+	}
+}
